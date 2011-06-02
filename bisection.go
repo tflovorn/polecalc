8f93663c9db99db276938a64593c85a19b9733bc
Tim Lovorn <tflovorn@crimson.ua.edu>
2011-06-01 19:31:17 -0500

    bisection algorithm iteration

---
+++ bisection.go
@@ -0,0 +1,43 @@
+package goroots
+
+type RootError struct {
+	Reason string
+}
+
+func NewRootError(reason string) *RootError {
+	error := RootError{reason}
+	return &error
+}
+
+func (e *RootError) String() string {
+	return e.Reason
+}
+
+type Func1D func(float64) float64
+
+func SolveBisection(f Func1D, left, right, epsilon float64) (float64, *RootError) {
+	if left > right {
+		left, right = right, left
+	}
+}
+
+func BisectionIterate(f Func1D, left, right float64) (float64, float64, *RootError) {
+	if f(left) * f(right) > 0 {
+		return left, right, NewRootError("arguments do not bracket a root")
+	}
+	midpoint := (left + right) / 2.0
+	fl, fm, fr = f(left), f(midpoint), f(right)
+	if fl == 0 {
+		return left, left, nil
+	} else if fr == 0 {
+		return right, right, nil
+	} else if (fl > 0 && fm < 0) || (fl < 0 && fm > 0) {
+		// root is in the left half
+		return left, midpoint, nil
+	} else if (fm > 0 && fr < 0) || (fm < 0 && fr > 0) {
+		// root is in the right half
+		return midpoint, right, nil
+	} else {
+		return midpoint, midpoint, nil
+	}
+}
