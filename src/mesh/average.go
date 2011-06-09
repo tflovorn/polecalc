fba451924f4f82cafc447e2b40e43a4651a5491d
Tim Lovorn <tflovorn@crimson.ua.edu>
2011-06-09 14:47:14 -0500

    restored makefiles for roots / mpljson.  added makefile for mesh.

--- src/mesh/average.go
+++ src/mesh/average.go
@@ -1,6 +1,4 @@
-package average
-
-import "./mesh2d"
+package mesh2d
 
 type Accumulator struct {
     value float64
@@ -33,7 +31,7 @@ type Consumer func(cmesh chan []float64, accum chan float64)
 // pointsPerSide is uint32 so that accum.points will fit in a uint64
 // numWorkers is uint16 to avoid spawning a ridiculous number of processes
 func Average(pointsPerSide uint32, worker Consumer, numWorkers uint16) float64 {
-    cmesh, done := mesh2d.Square(pointsPerSide)
+    cmesh, done := Square(pointsPerSide)
     accum := BuildAccumulator()
     var i uint16 = 0
     for i = 0; i < numWorkers; i++ {
