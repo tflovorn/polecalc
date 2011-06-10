b22d95997fdf69d74c56d07a1c31f6ccb54200e7
Tim Lovorn <tflovorn@crimson.ua.edu>
2011-06-10 16:23:31 -0500

    automated environment_test

--- src/polecalc/environment_test.go
+++ src/polecalc/environment_test.go
@@ -7,11 +7,14 @@ import (
 
 // Does Environment correctly convert to and from JSON?
 func TestEnvironmentConvert(t *testing.T) {
+	expectedEnv := "{\"GridLength\":64,\"InitD1\":0.1,\"InitMu\":0.1,\"InitF0\":0.1,\"Alpha\":-1,\"T0\":1,\"Tz\":0.1,\"Thp\":0.1,\"X\":0.1,\"D1\":0.1,\"Mu\":0.1,\"F0\":0.1}"
 	env, err := EnvironmentFromFile("environment_test.json")
 	if err != nil {
 		fmt.Println(err)
 		t.FailNow()
 	}
 	env.Initialize()
-	fmt.Println(env)
+	if env.String() != expectedEnv {
+		t.FailNow()
+	}
 }
