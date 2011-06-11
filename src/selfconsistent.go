4a96ea6497dea20cc88fa5112fda1dd813031792
Tim Lovorn <tflovorn@crimson.ua.edu>
2011-06-11 13:50:16 -0500

    changed references to Environment in selfconsistent to arbitrary type

--- src/selfconsistent.go
+++ src/selfconsistent.go
@@ -2,34 +2,36 @@ package polecalc
 
 import "os"
 
+type Arguments interface{}
+
 // One-parameter scalar self-consistent equation
 type SelfConsistentEquation interface {
-	// return the absolute error associated with this equation under the given Environment
-	AbsError(env Environment) float64
-	// set the appropriate variable in env to value
-	SetEnvironment(value float64, env *Environment)
-	// range of possible values for SetEnvironment
-	Range(env Environment) (float64, float64, os.Error)
+	// return the absolute error associated with this equation under the given Arguments
+	AbsError(args Arguments) float64
+	// set the appropriate variable in args to value
+	SetArguments(value float64, args *Arguments)
+	// range of possible values for SetArguments
+	Range(args Arguments) (float64, float64, os.Error)
 }
 
-// Return an Environment which solves eq to the given tolerance
-func Solve(eq SelfConsistentEquation, env Environment, tolerance float64) (Environment, os.Error) {
+// Return an Arguments which solves eq to the given tolerance
+func Solve(eq SelfConsistentEquation, args Arguments, tolerance float64) (Arguments, os.Error) {
 	eqError := func(value float64) float64 {
-		eq.SetEnvironment(value, &env)
-		return eq.AbsError(env)
+		eq.SetArguments(value, &args)
+		return eq.AbsError(args)
 	}
 	// if eq only has one root, left and right must bracket it
 	// --- todo: need to verify this ---
-	left, right, err := eq.Range(env)
+	left, right, err := eq.Range(args)
 	if err != nil {
-		return env, err
+		return args, err
 	}
 	solution, err := BisectionFullPrecision(eqError, left, right)
 	if err != nil {
-		return env, err
+		return args, err
 	}
-	eq.SetEnvironment(solution, &env)
-	return env, nil
+	eq.SetArguments(solution, &args)
+	return args, nil
 }
 
 // A group of self-consistent equations which may be coupled and must all be
@@ -39,22 +41,22 @@ type SelfConsistentSystem struct {
 	Tolerances []float64
 }
 
-// Solve the self-consistent system, returning the resulting Environment
-func (system *SelfConsistentSystem) Solve(env Environment) (Environment, os.Error) {
+// Solve the self-consistent system, returning the resulting Arguments
+func (system *SelfConsistentSystem) Solve(args Arguments) (Arguments, os.Error) {
 	i := 0
-	for !system.IsSolved(env) {
+	for !system.IsSolved(args) {
 		// this should never be true: if it is, failed to iterate
 		if i >= len(system.Equations) {
 			panic("self-consistent system overran bounds")
 		}
-		// set env to the value that solves the equation
-		newEnv, err := Solve(system.Equations[i], env, system.Tolerances[i])
+		// set args to the value that solves the equation
+		newEnv, err := Solve(system.Equations[i], args, system.Tolerances[i])
 		if err != nil {
-			return env, err
+			return args, err
 		}
-		env = newEnv
+		args = newEnv
 		// check if we need to iterate
-		if !system.solvedUpTo(env, i) {
+		if !system.solvedUpTo(args, i) {
 			// previous equations have been disturbed; restart
 			i = 0
 		} else {
@@ -62,16 +64,16 @@ func (system *SelfConsistentSystem) Solve(env Environment) (Environment, os.Erro
 			i++
 		}
 	}
-	return env, nil
+	return args, nil
 }
 
 // Check if the first (maxIndex + 1) equations are solved
-func (system *SelfConsistentSystem) solvedUpTo(env Environment, maxIndex int) bool {
+func (system *SelfConsistentSystem) solvedUpTo(args Arguments, maxIndex int) bool {
 	for i, eq := range system.Equations {
 		if i > maxIndex {
 			break
 		}
-		if eq.AbsError(env) > system.Tolerances[i] {
+		if eq.AbsError(args) > system.Tolerances[i] {
 			return false
 		}
 	}
@@ -79,9 +81,9 @@ func (system *SelfConsistentSystem) solvedUpTo(env Environment, maxIndex int) bo
 }
 
 // Are all the self-consistent equations solved?
-func (system *SelfConsistentSystem) IsSolved(env Environment) bool {
+func (system *SelfConsistentSystem) IsSolved(args Arguments) bool {
 	if len(system.Equations) == 0 {
 		return true
 	}
-	return system.solvedUpTo(env, len(system.Equations)-1)
+	return system.solvedUpTo(args, len(system.Equations)-1)
 }
