6ede52dcc0d68e82c7ffdca4366ce880630cae85
Tim Lovorn <tflovorn@crimson.ua.edu>
2011-06-26 16:54:36 -0500

    turned zerotemp_test down to GridLength 8

--- src/zerotemp_test.go
+++ src/zerotemp_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestKnownZeroTempSystem(t *testing.T) {
-	envStr := "{\"GridLength\":16,\"NumProcs\":1,\"InitD1\":0.1,\"InitMu\":0.1,\"InitF0\":0.1,\"Alpha\":-1,\"T0\":1,\"Tz\":0.1,\"Thp\":0.1,\"X\":0.1,\"D1\":0.052859796549526133,\"Mu\":-0.21741415401597314,\"F0\":0.13102201547770487,\"EpsilonMin\":-1.8}"
+	envStr := "{\"GridLength\":8,\"ImG0Bins\":0,\"NumProcs\":1,\"InitD1\":0.1,\"InitMu\":0.1,\"InitF0\":0.1,\"Alpha\":-1,\"J\":0,\"A\":0,\"T0\":1,\"Tz\":0.1,\"Thp\":0.1,\"X\":0.1,\"Lambda\":0,\"D1\":0.05777149373506872,\"Mu\":-0.18330570279347036,\"F0\":0.12945949461029926,\"EpsilonMin\":-1.8}"
 	expectedEnv, err := EnvironmentFromString(envStr)
 	if err != nil {
 		t.Fatal(err)
@@ -26,4 +26,5 @@ func TestKnownZeroTempSystem(t *testing.T) {
 	if !reflect.DeepEqual(solvedEnv, *expectedEnv) {
 		t.Fatalf("unknown solution to zero-temp system: got\n%s, expected\n%s", (&solvedEnv).String(), expectedEnv.String())
 	}
+	//	println(solvedEnv.ZeroTempErrors())
 }
