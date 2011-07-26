006c7003fcca8da5c3737149667ad357f8a32749
Tim Lovorn <tflovorn@crimson.ua.edu>
2011-07-26 01:16:39 -0500

    interface for GSL PV integral. compiles with cgo, untested

---
+++ src/principalvalue.go
@@ -0,0 +1,39 @@
+// Interface to GSL principal value integral functionality
+// (see http://www.gnu.org/software/gsl/manual/html_node/QAWC-adaptive-integration-for-Cauchy-principal-values.html)
+// Callback passing through cgo follows the model at:
+// http://stackoverflow.com/questions/6125683/call-go-functions-from-c/6147097#6147097
+package polecalc
+
+// #include <stdlib.h>
+// #include <gsl/gsl_integration.h>
+// extern double goEvaluate(double x, void* params);
+//
+// static double goPvIntegral(double a, double b, double c, double epsabs, double epsrel, size_t limit, void* userdata) {
+// 	gsl_integration_workspace *w = gsl_integration_workspace_alloc(limit);
+//	double result, error;
+//	gsl_function F;
+//	F.function = &goEvaluate;
+//	F.params = &userdata;
+//	gsl_integration_qawc(&F, a, b, c, epsabs, epsrel, limit, w, &result, &error);
+//	gsl_integration_workspace_free(w);
+//	return result;
+// }
+import "C"
+import "unsafe"
+
+type pvHandler func(x float64) float64
+
+type pvRequest struct {
+	f pvHandler
+}
+
+//export goEvaluate
+func goEvaluate(x C.double, userdata unsafe.Pointer) C.double {
+	req := (*pvRequest)(userdata)
+	return C.double(req.f(float64(x)))
+}
+
+func PvIntegralGSL(f Func1D, a, b, c, epsabs, epsrel float64, limit uint16) float64 {
+	req := unsafe.Pointer(&pvRequest{f})
+	return float64(C.goPvIntegral(C.double(a), C.double(b), C.double(c), C.double(epsabs), C.double(epsrel), C.size_t(limit), req))
+}
